b3e829c592b87dff8b8c80e5a0831e06d6cd95af
Grant Colegate <blaknite@thelanbox.com.au>
2025-12-16 00:56:20 +1030

    Connect it up to the internet

--- http.c
+++ http.c
@@ -9,10 +9,11 @@ struct http_client *http_client_table;
 uint8_t *mime_types[] = {
   "htm", "text/html",
   "txt", "text/plain",
-  "png", "image/png"
+  "png", "image/png",
+  NULL
 };
 
-uint8_t *text_types[] = { "htm", "txt" };
+uint8_t *text_types[] = { "htm", "txt", NULL };
 
 uint8_t *http_tx_buffer;
 
@@ -43,11 +44,11 @@ void http_system_response(struct http_client *c, uint16_t code, uint8_t *message
   uint8_t *ptr;
 
   ptr = &http_tx_buffer[0];
-  ptr += sprintf(ptr, "HTTP/1.0 %u %s\r\n", code, message);
-  ptr += sprintf(ptr, "Content-Type: text/html\r\n");
-  ptr += sprintf(ptr, "Content-Length: %u\r\n", 4 + strlen(message) + 2);
-  ptr += sprintf(ptr, "\r\n");
-  ptr += sprintf(ptr, "%u %s\r\n", code, message);
+  ptr += sprintf((char *)ptr, "HTTP/1.0 %u %s\r\n", code, (char *)message);
+  ptr += sprintf((char *)ptr, "Content-Type: text/html\r\n");
+  ptr += sprintf((char *)ptr, "Content-Length: %u\r\n", 4 + strlen((char *)message) + 2);
+  ptr += sprintf((char *)ptr, "\r\n");
+  ptr += sprintf((char *)ptr, "%u %s\r\n", code, (char *)message);
 
   http_log(c, code);
 
@@ -57,26 +58,36 @@ void http_system_response(struct http_client *c, uint16_t code, uint8_t *message
 uint8_t *http_content_type(struct http_client *c) {
   uint8_t ext[4];
   uint8_t i;
+  uint16_t len = strlen((char *)c->req_file);
 
-  strncpy(ext, &c->req_file[strlen(c->req_file) - 3], 4);
+  if (len < 3) {
+    return (uint8_t *)"application/octet-stream";
+  }
+
+  strncpy((char *)ext, (char *)&c->req_file[len - 3], 4);
 
   for (i = 0; mime_types[i]; i += 2) {
-    if (strcasecmp(ext, mime_types[i]) == 0) {
+    if (strcasecmp((char *)ext, (char *)mime_types[i]) == 0) {
       return mime_types[i + 1];
     }
   }
 
-  return "application/octet-stream";
+  return (uint8_t *)"application/octet-stream";
 }
 
 uint8_t http_file_mode(struct http_client *c) {
   uint8_t ext[4];
   uint8_t i;
+  uint16_t len = strlen((char *)c->req_file);
 
-  strncpy(ext, &c->req_file[strlen(c->req_file) - 3], 4);
+  if (len < 3) {
+    return HTTP_FILE_MODE_BINARY;
+  }
+
+  strncpy((char *)ext, (char *)&c->req_file[len - 3], 4);
 
   for (i = 0; text_types[i]; i++) {
-    if (strcasecmp(ext, text_types[i]) == 0) {
+    if (strcasecmp((char *)ext, (char *)text_types[i]) == 0) {
       return HTTP_FILE_MODE_TEXT;
     }
   }
@@ -86,9 +97,9 @@ uint8_t http_file_mode(struct http_client *c) {
 
 int16_t http_file_open(struct http_client *c) {
   if (c->file_mode == HTTP_FILE_MODE_TEXT) {
-    return open(&c->req_file[1], O_RDONLY, _IOTEXT);
+    return open((char *)&c->req_file[1], O_RDONLY, _IOTEXT);
   } else {
-    return open(&c->req_file[1], O_RDONLY, 0);
+    return open((char *)&c->req_file[1], O_RDONLY, 0);
   }
 }
 
@@ -126,10 +137,10 @@ void http_response(struct http_client *c) {
     c->tx_len = http_content_length(c, fd);
 
     ptr = &http_tx_buffer[0];
-    ptr += sprintf(ptr, "HTTP/1.0 200 OK\r\n");
-    ptr += sprintf(ptr, "Content-Type: %s\r\n", http_content_type(c));
-    ptr += sprintf(ptr, "Content-Length: %lu\r\n", c->tx_len);
-    ptr += sprintf(ptr, "\r\n");
+    ptr += sprintf((char *)ptr, "HTTP/1.0 200 OK\r\n");
+    ptr += sprintf((char *)ptr, "Content-Type: %s\r\n", (char *)http_content_type(c));
+    ptr += sprintf((char *)ptr, "Content-Length: %lu\r\n", c->tx_len);
+    ptr += sprintf((char *)ptr, "\r\n");
 
     http_log(c, 200);
 
@@ -137,49 +148,61 @@ void http_response(struct http_client *c) {
 
     close(fd);
   } else {
-    http_system_response(c, 404, "Not Found");
+    http_system_response(c, 404, (uint8_t *)"Not Found");
   }
 }
 
 void http_parse_request(struct http_client *c) {
+  int8_t *req_method;
   int8_t *req_file;
 
   if (c->rx_cur < 9) {
     return;
   }
 
-  if (strncmp(&c->rx_buff[c->rx_cur - 4], "\r\n\r\n", 4) != 0) {
+  if (strncmp((char *)&c->rx_buff[c->rx_cur - 4], "\r\n\r\n", 4) != 0) {
     return;
   }
 
-  strcpy(c->req_method, strtok(c->rx_buff, " "));
-  req_file = strtok(NULL, " ");
+  req_method = (int8_t *)strtok((char *)c->rx_buff, " ");
+  if (!req_method) {
+    http_system_response(c, 400, (uint8_t *)"Bad Request");
+    return;
+  }
 
-  if (strlen(req_file) > 14) {
-    http_system_response(c, 414, "URI Too Long");
+  req_file = (int8_t *)strtok(NULL, " ");
+  if (!req_file) {
+    http_system_response(c, 400, (uint8_t *)"Bad Request");
+    return;
+  }
+
+  if (strlen((char *)req_file) >= 14) {
+    http_system_response(c, 414, (uint8_t *)"URI Too Long");
     return;
   }
 
   if (req_file[0] != '/') {
-    http_system_response(c, 400, "Bad Request");
+    http_system_response(c, 400, (uint8_t *)"Bad Request");
     return;
   }
 
-  if (strchr(req_file, ":")) {
-    http_system_response(c, 400, "Bad Request");
+  if (strchr((char *)req_file, ':')) {
+    http_system_response(c, 400, (uint8_t *)"Bad Request");
     return;
   }
 
-  if (strlen(req_file) == 1) {
-    strcpy(c->req_file, "/INDEX.HTM");
+  strcpy((char *)c->req_method, (char *)req_method);
+
+  if (strlen((char *)req_file) == 1) {
+    strcpy((char *)c->req_file, "/INDEX.HTM");
   } else {
-    strcpy(c->req_file, req_file);
+    strcpy((char *)c->req_file, (char *)req_file);
   }
 
-  if (strncmp(c->req_method, "GET", 3) == 0 || strncmp(c->req_method, "HEAD", 4) == 0) {
+  if (strncmp((char *)c->req_method, "GET", 3) == 0 || strncmp((char *)c->req_method, "HEAD", 4) == 0) {
     http_response(c);
   } else {
-    http_system_response(c, 404, "Not Found");
+    http_system_response(c, 404, (uint8_t *)"Not Found");
   }
 }
 
@@ -210,7 +233,7 @@ void http_recv(struct tcp_sock *s, uint8_t *data, uint16_t len) {
   }
 
   if (c->rx_cur + len > HTTP_RX_LEN) {
-    http_system_response(c, 431, "Request Header Fields Too Large");
+    http_system_response(c, 431, (uint8_t *)"Request Header Fields Too Large");
     return;
   }
 
@@ -231,12 +254,12 @@ void http_send(struct tcp_sock *s, uint16_t len) {
 
   switch (c->state) {
     case HTTP_TX_HDR:
-      tcp_tx_data(c->s, http_tx_buffer, strlen(http_tx_buffer));
+      tcp_tx_data(c->s, http_tx_buffer, strlen((char *)http_tx_buffer));
       c->state = HTTP_TX_BODY;
       break;
 
     case HTTP_TX_BODY:
-      if (strncmp(c->req_method, "GET", 3) != 0) {
+      if (strncmp((char *)c->req_method, "GET", 3) != 0) {
         break;
       }
 
--- slip.h
+++ slip.h
@@ -1,8 +1,8 @@
 #ifndef __SLIP_H__
 #define __SLIP_H__
 
-#define SLIP_MTU 296 // TCP_PACKET_LEN plus headers
-#define SLIP_MAX 594 // SLIP_MTU * 2 + SLIP_END * 2
+#define SLIP_MTU 576 // Internet minimum MTU (RFC 791)
+#define SLIP_MAX 1154 // SLIP_MTU * 2 + SLIP_END * 2
 
 #define SLIP_END 0xc0
 #define SLIP_ESC 0xdb
@@ -11,10 +11,19 @@
 
 #define slip_buffer_alloc() (calloc(SLIP_MAX, 1))
 
+typedef struct {
+  uint8_t *buffer;
+  uint16_t length;
+  uint8_t escaped;
+} slip_decoder_t;
+
 extern uint8_t *slip_rx_buffer;
 extern uint8_t *slip_tx_buffer;
+extern slip_decoder_t slip_decoder;
 
 void slip_init(void);
+void slip_decoder_reset(void);
+uint8_t slip_process_byte(uint8_t b);
 void slip_rx(void);
 void slip_tx(uint8_t *buffer, uint16_t len);
 
